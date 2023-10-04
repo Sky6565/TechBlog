43ec4bb2c18cc65661bdd3a2058c9d77f9356793
sky6565 <godwin1988b@outlook.com>
2023-10-03 17:52:03 -0700

    took care of the Bug

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,15 +1,15 @@
-const router = require('express').Router();
-const { User, Comments, Post } = require('../models');
-const withAuth = require('../utils/auth');
+const router = require("express").Router();
+const { User, Comments, Post } = require("../models");
+const withAuth = require("../utils/auth");
 
-router.get('/profile', withAuth, async (req, res) => {
+router.get("/profile", withAuth, async (req, res) => {
   if (!req.session.logged_in) {
-    res.redirect('/');
+    res.redirect("/profile");
     return;
   }
   try {
-    const user = await User.findByPk(req.session.user_id, {include: "posts"});
-    res.render('profile', {
+    const user = await User.findByPk(req.session.user_id, { include: "posts" });
+    res.render("profile", {
       user: user.toJSON(),
       // Pass the logged in flag to the template
       logged_in: req.session.logged_in,
@@ -19,33 +19,32 @@ router.get('/profile', withAuth, async (req, res) => {
   }
 });
 
-router.get('/login', (req, res) => {
+router.get("/", (req, res) => {
   // If a session exists, redirect the request to the homepage
   if (req.session.logged_in) {
-    res.redirect('/');
+    res.redirect("/login");
     return;
   }
-  res.render('login');
+  res.render("login");
 });
 
-router.get('/signup', async (req, res) =>{
-  res.render('signup', {
+router.get("/signup", async (req, res) => {
+  res.render("signup", {
     logged_in: req.session.logged_in,
-  })
+  });
 });
 
-
-router.get('/profile/:id', async (req, res) => {
-  try{ 
-      const postData = await Post.findByPk(req.params.id);
-      if(!postData) {
-          res.status(404).json({message: 'No post with this id!'});
-          return;
-      }
-      const users = usersData.get({ plain: true });
-      res.render('profile', users);
-    } catch (err) {
-        res.status(500).json(err);
-    };     
+router.get("/profile/:id", async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id);
+    if (!postData) {
+      res.status(404).json({ message: "No post with this id!" });
+      return;
+    }
+    const users = usersData.get({ plain: true });
+    res.render("profile", users);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 module.exports = router;
